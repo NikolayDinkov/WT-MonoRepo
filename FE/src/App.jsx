27ff8115eb591565487110e25287c48389419c64
desiggancheva <desi3gan4eva@gmail.com>
2025-04-27 18:59:08 +0300

    commit try

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -1,15 +1,30 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom';
 import Home from './pages/Home';
 import FolderView from './pages/FolderView';
 
+function Layout() {
+  return (
+    <div className="home-page">
+      <header className="home-header">
+        <h1>My Drive</h1> {/* Само тук My Drive */}
+      </header>
+      <main className="home-main">
+        <Outlet />
+      </main>
+    </div>
+  );
+}
+
 function App() {
-  const userId = '60b8d2b8f5a8c72b4e5d5c5e'; // your test logged-in user
+  const userId = '60b8d2b8f5a8c72b4e5d5c5e';
 
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home userId={userId} />} />
-        <Route path="/folder/:folderId" element={<FolderView userId={userId} />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home userId={userId} />} />
+          <Route path="/folder/:folderId" element={<FolderView userId={userId} />} />
+        </Route>
       </Routes>
     </Router>
   );
