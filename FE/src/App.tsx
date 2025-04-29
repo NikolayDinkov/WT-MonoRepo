a153ba7d28a9c6fae5c2141456631be5365a0034
Lachezar Kunov <lachezarkunovbg@gmail.com>
2025-04-29 22:56:37 +0300

    combine login and register with app

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -8,11 +8,26 @@ import './App.css';
 import Sidebar from './components/Sidebar/Sidebar';
 import Header from './components/Header/Header';
 import MainContent from './components/MainContent/MainContent';
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
+
+import { getCurrentUser, logoutUser } from './LoginRegisterMock';
+
 import { useEffect, useState } from 'react';
 import { Element } from './interfaces/Element';
 
 const App = () => {
   const [myDrive, setMyDrive] = useState<Element[]>([]);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  function handleLogout() {
+    logoutUser();
+    setIsLoggedIn(false);
+  }
+
+  useEffect(() => {
+    setIsLoggedIn(!!getCurrentUser());
+  }, [isLoggedIn]);
 
   useEffect(() => {
     fetch('http://localhost:3000/elements/662fb8a1e9e4c7a29b123abc')
@@ -23,23 +38,31 @@ const App = () => {
 
   return (
     <Router>
-      <div className="app">
-        <Sidebar />
-        <div className="main-area">
-          <Header myDrive={myDrive} />
-          <Routes>
-            <Route
-              path="/my-drive/:directoryId?"
-              element={<MainContent page="myDrive" myDrive={myDrive} />}
-            />
-            <Route
-              path="/shared-with-me"
-              element={<MainContent page="shared" myDrive={myDrive} />}
-            />
-            <Route path="*" element={<Navigate to="/my-drive" replace />} />
-          </Routes>
+      {!isLoggedIn ? (
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="*" element={<Navigate to="/login" />} />
+        </Routes>
+      ) : (
+        <div className="app">
+          <Sidebar />
+          <div className="main-area">
+            <Header myDrive={myDrive} onLogout={handleLogout} />
+            <Routes>
+              <Route
+                path="/my-drive/:directoryId?"
+                element={<MainContent page="myDrive" myDrive={myDrive} />}
+              />
+              <Route
+                path="/shared-with-me"
+                element={<MainContent page="shared" myDrive={myDrive} />}
+              />
+              <Route path="*" element={<Navigate to="/my-drive" replace />} />
+            </Routes>
+          </div>
         </div>
-      </div>
+      )}
     </Router>
   );
 };
