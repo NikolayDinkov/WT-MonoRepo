7cf9893d4b76f2bf248967e558faf051d86c28c9
Lachezar Kunov <lachezarkunovbg@gmail.com>
2025-06-05 15:12:13 +0300

    fetch files from userId, not from hardcoded one

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -12,19 +12,34 @@ import { useEffect, useState } from 'react';
 import { Element } from './interfaces/Element';
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
+import { jwtDecode } from 'jwt-decode';
+import JwtPayload from './interfaces/JwtPayload';
 
 const App = () => {
   const [myDrive, setMyDrive] = useState<Element[]>([]);
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+  const [userId, setUserId] = useState<string | null>(null);
 
   useEffect(() => {
     const token = localStorage.getItem('token');
-    setIsLoggedIn(!!token);
+    if (token) {
+      try {
+        const decoded = jwtDecode<JwtPayload>(token);
+        setUserId(decoded.userId);
+        setIsLoggedIn(true);
+      } catch (error) {
+        setIsLoggedIn(false);
+        setUserId(null);
+      }
+    } else {
+      setIsLoggedIn(false);
+      setUserId(null);
+    }
   }, []);
 
   useEffect(() => {
-    if (isLoggedIn) {
-      fetch('http://localhost:3000/elements/662fb8a1e9e4c7a29b123abc', {
+    if (isLoggedIn && userId) {
+      fetch(`http://localhost:3000/elements/${userId}`, {
         headers: {
           Authorization: `Bearer ${localStorage.getItem('token')}`,
         },
@@ -33,7 +48,7 @@ const App = () => {
         .then((res) => setMyDrive(res))
         .catch((error) => console.error(error));
     }
-  }, [isLoggedIn]);
+  }, [isLoggedIn, userId]);
 
   return (
     <Router>
