7b0dab800c6aa674bf5dfe0c36e1578d21519f63
desiggancheva <desi3gan4eva@gmail.com>
2025-06-07 13:59:16 +0300

    merge

--- FE/src/components/MyDrive/MyDrive.tsx
+++ FE/src/components/MyDrive/MyDrive.tsx
@@ -7,7 +7,6 @@ import { useFileContext } from '../../contexts/fileContext';
 const MyDrive: React.FC = () => {
   const { directoryId } = useParams<{ directoryId: string }>();
   const { myDrive } = useFileContext();
-  const { myDrive } = useFileContext();
   const currentPath: string | null = directoryId || null;
   const navigate = useNavigate();
 
